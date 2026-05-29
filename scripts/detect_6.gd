721e5d17b62fe95d7e78cac81ef425f86b4360fc
NEENmzd <nick0625red@gmail.com>
2026-05-29 19:15:38 +0800

    8451

--- scripts/detect_6.gd
+++ scripts/detect_6.gd
@@ -1,12 +1,9 @@
 extends Area2D
 
 @export var fall_force := 100.0
-@export var wall5: StaticBody2D
 
 var bodies := []
 
-func _ready():
-	wall5.disable_wall()
 
 func _physics_process(_delta):
 	for body in bodies:
@@ -16,7 +13,6 @@ func _physics_process(_delta):
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		bodies.append(body)
-		wall5.enable_wall()
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):
